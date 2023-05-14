d56ea55f77a065b0ab89642624d8ffaf01024870
shehatacs2019102 <119540983+shehatacs2019102@users.noreply.github.com>
2023-05-14 18:52:17 +0300

    commit

--- app/Models/FoodItem.php
+++ app/Models/FoodItem.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+//this is a change
 class FoodItem extends Model
 {
     use HasFactory;
